2b663d82783347ba172bf497cf290162dca407ae
etf-investments <etf-investments@github.com>
2018-05-25 13:32:55 -0400

    more highlighting - css smaller

--- etf_inv_lib.js
+++ etf_inv_lib.js
@@ -1,5 +1,9 @@
   'use strict';
 
+  var vwap_curve = {"09:30":0.04135673,"09:31":0.04735759,"09:32":0.05439223,"09:33":0.0617591,"09:34":0.0684682,"09:35":0.0776367,"09:36":0.08569758,"09:37":0.09280652,"09:38":0.09870697,"09:39":0.1054056,"09:40":0.1114267,"09:41":0.1169948,"09:42":0.1226002,"09:43":0.1284586,"09:44":0.1338585,"09:45":0.1400729,"09:46":0.1455645,"09:47":0.1500898,"09:48":0.1560633,"09:49":0.161353,"09:50":0.1672916,"09:51":0.1727147,"09:52":0.1808765,"09:53":0.1874793,"09:54":0.193578,"09:55":0.1991718,"09:56":0.2041314,"09:57":0.2085836,"09:58":0.2123864,"09:59":0.2157984,"10:00":0.2205345,"10:01":0.2254695,"10:02":0.2291975,"10:03":0.2331902,"10:04":0.2371917,"10:05":0.241198,"10:06":0.2448558,"10:07":0.2494315,"10:08":0.2529616,"10:09":0.2576271,"10:10":0.2617713,"10:11":0.2668017,"10:12":0.2702197,"10:13":0.2732382,"10:14":0.2770127,"10:15":0.2804246,"10:16":0.2839557,"10:17":0.2875156,"10:18":0.2899736,"10:19":0.2928108,"10:20":0.2952845,"10:21":0.2980184,"10:22":0.3011051,"10:23":0.3037269,"10:24":0.3061084,"10:25":0.3086235,"10:26":0.3112154,"10:27":0.3138188,"10:28":0.3163048,"10:29":0.31882,"10:30":0.3219103,"10:31":0.3253048,"10:32":0.3287988,"10:33":0.3316559,"10:34":0.3340363,"10:35":0.3368686,"10:36":0.3393076,"10:37":0.3423468,"10:38":0.3458235,"10:39":0.3493659,"10:40":0.352157,"10:41":0.3545967,"10:42":0.357169,"10:43":0.3594818,"10:44":0.361784,"10:45":0.3640432,"10:46":0.366524,"10:47":0.3687509,"10:48":0.371049,"10:49":0.373531,"10:50":0.3757571,"10:51":0.3779403,"10:52":0.3800615,"10:53":0.3824321,"10:54":0.3867957,"10:55":0.3887369,"10:56":0.3911429,"10:57":0.3935379,"10:58":0.3958582,"10:59":0.3982088,"11:00":0.4010345,"11:01":0.4036302,"11:02":0.405741,"11:03":0.4091397,"11:04":0.4115241,"11:05":0.4139819,"11:06":0.4161688,"11:07":0.4187109,"11:08":0.4207642,"11:09":0.4239059,"11:10":0.4261508,"11:11":0.4282752,"11:12":0.430023,"11:13":0.4320941,"11:14":0.4343259,"11:15":0.436915,"11:16":0.4397091,"11:17":0.4421465,"11:18":0.4449568,"11:19":0.4474033,"11:20":0.4494249,"11:21":0.4518632,"11:22":0.455289,"11:23":0.4577984,"11:24":0.4605711,"11:25":0.4625714,"11:26":0.4649407,"11:27":0.4668946,"11:28":0.4687469,"11:29":0.4712092,"11:30":0.4735095,"11:31":0.4755856,"11:32":0.4777755,"11:33":0.4804379,"11:34":0.4827125,"11:35":0.4852869,"11:36":0.4876255,"11:37":0.4897547,"11:38":0.4917418,"11:39":0.4954189,"11:40":0.4982349,"11:41":0.5005219,"11:42":0.5027945,"11:43":0.5051029,"11:44":0.5068798,"11:45":0.5082893,"11:46":0.5101546,"11:47":0.5119863,"11:48":0.5131168,"11:49":0.514831,"11:50":0.5166731,"11:51":0.5192026,"11:52":0.5208125,"11:53":0.5223166,"11:54":0.5237263,"11:55":0.5251127,"11:56":0.5261028,"11:57":0.52731,"11:58":0.5284093,"11:59":0.5301519,"12:00":0.5314971,"12:01":0.5327524,"12:02":0.5342135,"12:03":0.5359536,"12:04":0.5375849,"12:05":0.5394514,"12:06":0.5407304,"12:07":0.5418643,"12:08":0.5431162,"12:09":0.544053,"12:10":0.5454456,"12:11":0.5466715,"12:12":0.5482907,"12:13":0.5498513,"12:14":0.5514381,"12:15":0.553123,"12:16":0.5551558,"12:17":0.5563623,"12:18":0.5580241,"12:19":0.5597374,"12:20":0.560819,"12:21":0.5627193,"12:22":0.5647657,"12:23":0.5659331,"12:24":0.5677982,"12:25":0.5699806,"12:26":0.5727494,"12:27":0.574506,"12:28":0.5760058,"12:29":0.5778656,"12:30":0.5795001,"12:31":0.5813182,"12:32":0.5832487,"12:33":0.5858064,"12:34":0.5879833,"12:35":0.5897008,"12:36":0.5909358,"12:37":0.5923165,"12:38":0.5935425,"12:39":0.5952107,"12:40":0.5969357,"12:41":0.5988788,"12:42":0.6007925,"12:43":0.6032476,"12:44":0.6056642,"12:45":0.6074428,"12:46":0.6091486,"12:47":0.6106797,"12:48":0.6121057,"12:49":0.6135036,"12:50":0.6150578,"12:51":0.616753,"12:52":0.6182283,"12:53":0.6200347,"12:54":0.6213593,"12:55":0.6229925,"12:56":0.6242142,"12:57":0.6257329,"12:58":0.6268618,"12:59":0.6281145,"13:00":0.6294806,"13:01":0.6310348,"13:02":0.6326661,"13:03":0.6339624,"13:04":0.6351877,"13:05":0.6362828,"13:06":0.6376705,"13:07":0.6391631,"13:08":0.6404394,"13:09":0.6419504,"13:10":0.6430318,"13:11":0.6441842,"13:12":0.6456125,"13:13":0.6471092,"13:14":0.6484982,"13:15":0.649799,"13:16":0.6508374,"13:17":0.6518218,"13:18":0.6526549,"13:19":0.6536977,"13:20":0.6546316,"13:21":0.6564203,"13:22":0.6576968,"13:23":0.6606495,"13:24":0.6617535,"13:25":0.6628753,"13:26":0.6637627,"13:27":0.6645877,"13:28":0.6658621,"13:29":0.6670391,"13:30":0.6680015,"13:31":0.6693946,"13:32":0.6706036,"13:33":0.6716537,"13:34":0.672992,"13:35":0.6743316,"13:36":0.6753489,"13:37":0.6769238,"13:38":0.6779353,"13:39":0.6796053,"13:40":0.6811268,"13:41":0.6825638,"13:42":0.6838723,"13:43":0.6851938,"13:44":0.6865459,"13:45":0.6878453,"13:46":0.6889214,"13:47":0.6902485,"13:48":0.6914752,"13:49":0.6924046,"13:50":0.6953502,"13:51":0.6978844,"13:52":0.6990684,"13:53":0.7003945,"13:54":0.70149,"13:55":0.703073,"13:56":0.7044896,"13:57":0.7065191,"13:58":0.7074731,"13:59":0.7086455,"14:00":0.7109859,"14:01":0.7134772,"14:02":0.7154301,"14:03":0.7173473,"14:04":0.7198094,"14:05":0.7219406,"14:06":0.724351,"14:07":0.726386,"14:08":0.7274205,"14:09":0.7286018,"14:10":0.730372,"14:11":0.7321943,"14:12":0.7333154,"14:13":0.7354289,"14:14":0.7378572,"14:15":0.7398815,"14:16":0.7420151,"14:17":0.743959,"14:18":0.7461735,"14:19":0.7476992,"14:20":0.7489862,"14:21":0.7502077,"14:22":0.7520177,"14:23":0.7534298,"14:24":0.7554589,"14:25":0.7570404,"14:26":0.7580312,"14:27":0.7595909,"14:28":0.7634093,"14:29":0.7654767,"14:30":0.7671847,"14:31":0.7695821,"14:32":0.7716602,"14:33":0.773268,"14:34":0.7755097,"14:35":0.7767515,"14:36":0.7781936,"14:37":0.7799895,"14:38":0.7809417,"14:39":0.782169,"14:40":0.7834159,"14:41":0.7846366,"14:42":0.7863168,"14:43":0.7874543,"14:44":0.7888722,"14:45":0.7904776,"14:46":0.7919978,"14:47":0.793965,"14:48":0.7954745,"14:49":0.7969992,"14:50":0.7981653,"14:51":0.7991882,"14:52":0.8003651,"14:53":0.8016739,"14:54":0.8026294,"14:55":0.803913,"14:56":0.8053845,"14:57":0.8066629,"14:58":0.8079738,"14:59":0.8104947,"15:00":0.8120976,"15:01":0.8133292,"15:02":0.814872,"15:03":0.8169985,"15:04":0.818817,"15:05":0.8212172,"15:06":0.8242221,"15:07":0.8259815,"15:08":0.8281331,"15:09":0.8295275,"15:10":0.8313647,"15:11":0.8332055,"15:12":0.8350774,"15:13":0.8370567,"15:14":0.8387321,"15:15":0.8402908,"15:16":0.8437638,"15:17":0.8466646,"15:18":0.8488516,"15:19":0.8511217,"15:20":0.8531398,"15:21":0.8556906,"15:22":0.8581996,"15:23":0.8602864,"15:24":0.864265,"15:25":0.866534,"15:26":0.8687159,"15:27":0.8705433,"15:28":0.8726343,"15:29":0.8749369,"15:30":0.8779084,"15:31":0.8808782,"15:32":0.8839491,"15:33":0.886394,"15:34":0.888406,"15:35":0.8910063,"15:36":0.8930999,"15:37":0.895201,"15:38":0.8979506,"15:39":0.9003978,"15:40":0.9029867,"15:41":0.9056564,"15:42":0.9081363,"15:43":0.9109901,"15:44":0.9144858,"15:45":0.9179045,"15:46":0.9214515,"15:47":0.9243745,"15:48":0.9273846,"15:49":0.9303613,"15:50":0.938324,"15:51":0.9420762,"15:52":0.9460722,"15:53":0.9509272,"15:54":0.955361,"15:55":0.961374,"15:56":0.9668892,"15:57":0.9739224,"15:58":0.9805966,"15:59":0.9999953,"16:00":1};
+  var sortableLogicAdded = false;
+  var curTime = '09:30';
+
   var range  = r => Array.apply(null, Array(r)).map(function (_, i) {return i;});
 
   function addSortableLogic() {
@@ -12,6 +16,7 @@
         th[i2].sortDir = 'desc';        
        }
     }
+    sortableLogicAdded = true;
   }      
 
 function loadPrices(){
@@ -19,7 +24,7 @@ function loadPrices(){
       symbols = symbols.filter((s, i) => symbols.indexOf(s) === i);
       updateData('addTitle');
       setInterval(updateData, REFRESH_SECONDS * 1000);
-      addSortableLogic();
+
 };
 
 
@@ -93,6 +98,7 @@ function addPortfolio(portfolio, portfolio_index) {
         <th class="stock-change-pct">Change%</th>
         <th class="stock-latest-volume">Volume</th>
         <th class="stock-avg-total-volume">ADV</th>
+        <th class="stock-volume-projection">VolProj</th>
         <th class="stock-mkt-cap">AUM</th>
         <th class="flow-yesterday">Flow T-1</th>
         <th class="flow-1m">Flow 1m</th>
@@ -117,12 +123,13 @@ function addPortfolio(portfolio, portfolio_index) {
         <td class="stock-change-pct"></td>
         <td class="stock-latest-volume"></td>
         <td class="stock-avg-total-volume"></td>
+        <td class="stock-volume-projection"></td>
         <td class="stock-mkt-cap"></td>
         <td class="flow-yesterday" original_numeric=${item.flowYesterday}>${signedFlows(item.flowYesterday)}</td>
         <td class="flow-1m" original_numeric=${item.flow1m}>${signedFlows(item.flow1m)}</td>
-        <td class="stock-change-pct-1m" original_numeric=${item.tr1m}>${formatPercent(item.tr1m)}</td>
+        <td class="stock-change-pct-1m" original_numeric=${item.tr1m}>${formatPercent(item.tr1m,1)}</td>
         <td class="flow-3m" original_numeric=${item.flow3m}>${signedFlows(item.flow3m)}</td> 
-        <td class="stock-change-pct-3m" original_numeric=${item.tr3m}>${formatPercent(item.tr3m)}</td>                           
+        <td class="stock-change-pct-3m" original_numeric=${item.tr3m}>${formatPercent(item.tr3m,1)}</td>                           
       </tr>
     `
 
@@ -140,12 +147,55 @@ function addPortfolio(portfolio, portfolio_index) {
   `;
 
   containerDiv.appendChild(portfolioDiv);
+
+  document.querySelectorAll('tbody').forEach(t => {
+   t.querySelectorAll('tr').forEach(r => {
+      flowColoring(r,'1m');
+      flowColoring(r,'3m');
+   })
+  });
+
 }
 
+function flowColoring(r,grouping) {
+      var a = r.querySelectorAll(`.flow-${grouping}`)[0];
+      var b = r.querySelectorAll(`.stock-change-pct-${grouping}`)[0];
+      var a_sign = Math.sign(a.getAttribute('original_numeric'));
+      var b_sign = Math.sign(b.getAttribute('original_numeric'));
+
+      if( a_sign === b_sign ) {
+        let rgbColor = '0,255,0';
+        if( a_sign < 0 ){
+          rgbColor = '255,0,0';
+        }
+        let rgbOpacity = 0.1;
+        a.setAttribute('style', `background-color: rgba(${rgbColor}, ${rgbOpacity})`);
+        b.setAttribute('style', `background-color: rgba(${rgbColor}, ${rgbOpacity})`);
+      }
+};
+
+function getCurrentEST() {
+  var options = {
+      timeZone: 'America/New_York',
+      hour: '2-digit', minute: '2-digit',
+      hour12: false,
+  };
+  var formatter = new Intl.DateTimeFormat([], options);
+  var time = formatter.format(new Date());
+  if( time < '09:30' ){
+    return '09:30';
+  }
+  if(time > '16:00'){
+    return '16:00'
+  }
+  return time;
+}
 
 
 function updateData(addTitle) {
   let numberOfBatches = Math.ceil(symbols.length / BATCH_SIZE);
+
+  curTime = getCurrentEST();
 
   for (let i = 0; i < numberOfBatches; i++) {
     let symbolsBatch = symbols.slice(i * BATCH_SIZE, (i + 1) * BATCH_SIZE);
@@ -153,6 +203,14 @@ function updateData(addTitle) {
   }
 
   updatedDiv.innerHTML = `Data updated at ${(new Date()).toLocaleString()}`;
+
+  if(!sortableLogicAdded) {
+    addSortableLogic();
+  }
+
+  document.querySelectorAll('table').forEach(t => {
+    sortTable(t.id,4);
+  })
 }
 
 function sortTable(table_id,col_num) {
@@ -222,6 +280,7 @@ function updateDataForBatch(symbols, addTitle) {
   if (addTitle) filters.push('companyName');
   let url = `${BASE_URL}?types=quote&symbols=${symbols.join(',')}&filter=${filters.join(',')}`;
 
+
   fetch(url).then(response => response.json()).then(json => {
     symbols.forEach(symbol => {
       let data = json[symbol];
@@ -229,9 +288,12 @@ function updateDataForBatch(symbols, addTitle) {
 
       let formattedPrice = formatQuote(data.quote.latestPrice);
       let formattedChange = data.quote.change.toLocaleString('en', {'minimumFractionDigits': 2});
-      let formattedChangePercent = formatPercent(data.quote.changePercent);
+      let formattedChangePercent = formatPercent(data.quote.changePercent,2);
       let formattedLatestVolume = formatNumberToWords(data.quote.latestVolume);
       let formattedAvgTotalVolume = formatNumberToWords(data.quote.avgTotalVolume)
+
+      let volProjection = (data.quote.latestVolume/data.quote.avgTotalVolume)/vwap_curve[curTime];
+      let formattedVolProjection = formatPercent(volProjection,0);
 
 
       let formattedMarketCap = '$' + formatNumberToWords(data.quote.marketCap);
@@ -274,11 +336,16 @@ function updateDataForBatch(symbols, addTitle) {
         e.setAttribute('style', `background-color: rgba(${rgbColor}, ${rgbOpacity})`);
       });
 
-      if (addTitle) {
-        document.querySelectorAll(`[data-symbol="${symbol}"] .stock-symbol a`).forEach(e => {
-          e.setAttribute('title', data.quote.companyName);
-        });
-      }
+      document.querySelectorAll(`[data-symbol="${symbol}"] .stock-volume-projection`).forEach(e => {
+        e.innerHTML = formattedVolProjection;
+        e.originalNumeric = volProjection;
+        e.setAttribute('style', `background-color: rgba(${rgbColor}, ${rgbOpacity})`);
+      });
+
+      document.querySelectorAll(`[data-symbol="${symbol}"] .flow-yesterday`).forEach(e => {
+        e.setAttribute('style', `background-color: rgba(${rgbColor}, ${rgbOpacity})`);
+      });      
+
     });
   });
 }
@@ -309,8 +376,8 @@ function formatQuote(value) {
   return value.toLocaleString('en', options);
 }
 
-function formatPercent(num) {
-  return (num* 100).toFixed(1) + '%';
+function formatPercent(num,precision) {
+  return (num* 100).toFixed(precision) + '%';
 }
 
 function signedFlows(flowNumber) {
